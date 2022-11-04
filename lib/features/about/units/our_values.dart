7bd1175bd8fb421a8a9b9dc9e69225b9e75015cb
Mohamed Khalifa <mnpel789f@gmail.com>
2022-11-04 22:04:28 +0200

    home commit

--- lib/features/about/units/our_values.dart
+++ lib/features/about/units/our_values.dart
@@ -1,12 +1,13 @@
 part of '../view.dart';
+
 class Values extends StatelessWidget {
   const Values({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding:  EdgeInsets.symmetric(horizontal: 15.w),
-      child: Column(children:[
+      padding: EdgeInsets.symmetric(horizontal: 15.w),
+      child: Column(children: [
         Padding(
           padding: EdgeInsets.only(top: 38.h),
           child: CustomText(
@@ -25,24 +26,25 @@ class Values extends StatelessWidget {
             fontWeight: FontWeight.w600,
           ),
         ),
-        ValueContainer(image: 'assets/images/Rectangle 9.png',title: 'Post Loads and Vehicles',desc:
-            'User can post loads and vehicles from one location to another and contact other users directly.',
-            ),
-        ValueContainer(image: 'assets/images/Rectangle 9 (1).png',title: 'List items for Sale & Purchase',
-        desc: 'Different items related to logistics can be posted with it\'s details for sale or purchase.',
-
+        ValueContainer(
+          image: 'assets/images/Rectangle 9.png',
+          title: 'Post Loads and Vehicles',
+          desc:
+              'User can post loads and vehicles from one location to another and contact other users directly.',
         ),
-        ValueContainer(image: 'assets/images/Rectangle 9 (2).png',title: 'Publish your Job Openings',
-        desc: 'All available jobs can be published with specific role and designation.',
-
+        ValueContainer(
+          image: 'assets/images/Rectangle 9 (1).png',
+          title: 'List items for Sale & Purchase',
+          desc:
+              'Different items related to logistics can be posted with it\'s details for sale or purchase.',
         ),
-
-
-
-
+        ValueContainer(
+          image: 'assets/images/Rectangle 9 (2).png',
+          title: 'Publish your Job Openings',
+          desc:
+              'All available jobs can be published with specific role and designation.',
+        ),
       ]),
     );
   }
 }
-
-
